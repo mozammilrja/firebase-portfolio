97ebcdb10ffc31e1bc0c65094d7db60137fb437f
mozammilrja <44997979+mozammilrja@users.noreply.github.com>
2021-03-20 15:54:16 +0530

    fisrst commit

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -102,7 +102,7 @@ const Vision = () => {
                 impedit.
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
                 maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
+                impedit
               </p>
             </div>
           </div>
