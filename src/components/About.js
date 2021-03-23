ee3ea9908d3af39fea0ec47a32c874c82aeb0e90
mozammilrja <44997979+mozammilrja@users.noreply.github.com>
2021-03-23 09:40:24 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -49,10 +49,13 @@ const About = () => {
         <div className="container">
           <h5 className="main-heading">About Me</h5>
           <p>
-            Software Development Student | Seeking Full-Time Software
-            Engineering Job |ReactJS, JavaScript, CSS, HTML |2020Graduate
-          </p>
-          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
